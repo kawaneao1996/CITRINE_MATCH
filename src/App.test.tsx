a728b69cc2740c3dbf2381fa6115d501f161ccb0
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2023-11-25 01:09:59 +0900

    テスト環境構築まで。次はcalculateScoresのテストを書く

---
+++ src/App.test.tsx
@@ -0,0 +1,4 @@
+import { expect, test } from 'vitest'
+import App from './App'
+
+test("何もしないテスト", () => { });
--- src/App.tsx
+++ src/App.tsx
@@ -23,8 +23,40 @@ function App() {
   };
   const quizItems: QuizItem[] = [];
 
+  function calculateScore(quizItems: QuizItem[]): EvaluationCriteria | false {
+    const notNullScores: Record<keyof EvaluationCriteria, number[]> = {
+      selfEsteemLevel: [],
+      extroversionLevel: [],
+      energyEmissionLevel: [],
+    };
+    const resultScores: Record<keyof EvaluationCriteria, number | null> = {
+      selfEsteemLevel: null,
+      extroversionLevel: null,
+      energyEmissionLevel: null,
+    };
+    for (const item of quizItems) {
+      if (item.answer === undefined) {
+        return false;
+      } else {
+        Object.keys(item.answer.evaluationScore).forEach(key => {
+          const score = item.answer!.evaluationScore[key as keyof EvaluationCriteria];
+          if (score !== null) {
+            notNullScores[key as keyof EvaluationCriteria].push(score);
+          }
+        });
+        for (const key in notNullScores) {
+          const scores = notNullScores[key as keyof EvaluationCriteria];
+          if (scores.length > 0) {
+            const sum = scores.reduce((a, b) => a + b, 0);
+            resultScores[key as keyof EvaluationCriteria] = sum / scores.length;
+          }
+        }
+      }
+    }
+    return resultScores;
+  }
   const onSubmit = () => {
-    console.log(quizItems);
+    console.log(calculateScore(quizItems));
   };
 
   return (
@@ -34,7 +66,7 @@ function App() {
           <section key={index}>
             <article>{quizItem.question}</article>
             {Object.entries(quizItem.choices).map(([key, value]) => (
-              <button key={key} onClick={() => { quizItem.answer = Number(key) }}>{value}</button>
+              <button key={key} onClick={onSubmit}>{value.sentence}</button>
             ))}
           </section>
         ))
@@ -43,5 +75,6 @@ function App() {
     </>
   );
 }
+
 
 export default App
