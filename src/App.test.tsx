858340a4a47d3a76c701720f92afb39116475c31
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2023-11-25 10:16:58 +0900

    とりあえず正常系が動いたので

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,159 @@
-import { expect, test } from 'vitest'
-import App from './App'
+// import { expect, test } from 'vitest'
+// import App from './App'
 
-test("何もしないテスト", () => { });
+// test("何もしないテスト", () => { });
+
+// import { calculateScore } from './App';
+// import QuizItem from './App'
+// import EvaluationCriteria from './App'
+
+// // 正常系のデータ
+// const quizItemsValid: QuizItem[] = [
+//     {
+//         question: "Question 1",
+//         choices: [
+//             {
+//                 sentence: "Choice 1",
+//                 evaluationScore: {
+//                     selfEsteemLevel: 0.1,
+//                     extroversionLevel: 0.2,
+//                     energyEmissionLevel: 0.3
+//                 }
+//             },
+//             {
+//                 sentence: "Choice 2",
+//                 evaluationScore: {
+//                     selfEsteemLevel: 0.1,
+//                     extroversionLevel: 0.2,
+//                     energyEmissionLevel: 0.3
+//                 }
+//             },
+//             // 必要に応じて他の選択肢を追加
+//         ],
+//         answer:
+//         {
+//             sentence: "Choice 1",
+//             evaluationScore: {
+//                 selfEsteemLevel: 0.1,
+//                 extroversionLevel: 0.2,
+//                 energyEmissionLevel: 0.3
+//             }
+//         },
+//     },
+//     {
+//         question: "Question 2",
+//         choices: [
+//             {
+//                 sentence: "Choice 1",
+//                 evaluationScore: {
+//                     selfEsteemLevel: 0.4,
+//                     extroversionLevel: 0.5,
+//                     energyEmissionLevel: 0.6
+//                 }
+//             },
+//             // 必要に応じて他の選択肢を追加
+//         ],
+//         answer:
+//         {
+//             sentence: "Choice 1",
+//             evaluationScore: {
+//                 selfEsteemLevel: 0.4,
+//                 extroversionLevel: 0.5,
+//                 energyEmissionLevel: 0.6
+//             }
+//         },
+//     },
+//     {
+//         question: "Question 3",
+//         choices: [
+//             {
+//                 sentence: "Choice 1",
+//                 evaluationScore: {
+//                     selfEsteemLevel: -0.8,
+//                     extroversionLevel: 0.8,
+//                     energyEmissionLevel: 0.9
+//                 }
+//             },
+//             // 必要に応じて他の選択肢を追加
+//         ],
+//         answer:
+//         {
+//             sentence: "Choice 1",
+//             evaluationScore: {
+//                 selfEsteemLevel: -0.8,
+//                 extroversionLevel: 0.8,
+//                 energyEmissionLevel: 0.9
+//             }
+//         },
+//     }
+// ];
+// test('calculateScore correctly calculates scores', () => {
+
+//     const expected: EvaluationCriteria = {
+//         // ここに期待される結果を追加します
+//         selfEsteemLevel: -0.1,
+//         extroversionLevel: 0.5,
+//         energyEmissionLevel: 0.6
+//     };
+
+//     const result = calculateScore(quizItemsValid);
+//     expect(result).toEqual(expected);
+// });
+
+// // 異常系のデータ(answerにnull)
+// const quizItemsInvalid: QuizItem[] = [
+//     {
+//         question: "Question 1",
+//         choices: [
+//             {
+//                 sentence: "Choice 1",
+//                 evaluationScore: {
+//                     selfEsteemLevel: 0.1,
+//                     extroversionLevel: 0.2,
+//                     energyEmissionLevel: 0.3
+//                 }
+//             },
+//             // 必要に応じて他の選択肢を追加
+//         ],
+//         answer: null
+//     },
+//     {
+//         question: "Question 2",
+//         choices: [
+//             {
+//                 sentence: "Choice 1",
+//                 evaluationScore: {
+//                     selfEsteemLevel: 0.4,
+//                     extroversionLevel: 0.5,
+//                     energyEmissionLevel: 0.6
+//                 }
+//             },
+//             // 必要に応じて他の選択肢を追加
+//         ],
+//         answer: null
+//     },
+//     {
+//         question: "Question 3",
+//         choices: [
+//             {
+//                 sentence: "Choice 1",
+//                 evaluationScore: {
+//                     selfEsteemLevel: 0.7,
+//                     extroversionLevel: 0.8,
+//                     energyEmissionLevel: 0.9
+//                 }
+//             },
+//             // 必要に応じて他の選択肢を追加
+//         ],
+//         answer: null
+//     }
+// ];
+
+// test('calculateScore returns false if any answer is null', () => {
+//     // const quizItems: QuizItem[] = [
+//     //     // ここに、少なくとも1つのanswerがnullのテストデータを追加します
+//     // ];
+
+//     // const result = calculateScore(quizItems);
+//     // expect(result).toBe(false);
+// });
--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ function App() {
   type QuizItem = {
     question: string,
     choices: Choice[],
-    answer?: Choice,
+    // 解答前はnullが入っている
+    answer: Choice | null,
   };
   type EvaluationCriteria = {
     // スコアは -1.0 ~ 1.0 で付ける
@@ -21,8 +22,62 @@ function App() {
     // 評価項目の値
     evaluationScore: EvaluationCriteria,
   };
-  const quizItems: QuizItem[] = [];
-
+  const quizItems: QuizItem[] = [
+    {
+      question: "Question 1",
+      choices: [
+        {
+          sentence: "Choice 1",
+          evaluationScore: {
+            selfEsteemLevel: 0.1,
+            extroversionLevel: 0.2,
+            energyEmissionLevel: 0.3
+          }
+        },
+        // 必要に応じて他の選択肢を追加
+      ],
+      answer: null
+    },
+    {
+      question: "Question 2",
+      choices: [
+        {
+          sentence: "Choice 1",
+          evaluationScore: {
+            selfEsteemLevel: 0.4,
+            extroversionLevel: 0.5,
+            energyEmissionLevel: 0.6
+          }
+        },
+        // 必要に応じて他の選択肢を追加
+      ],
+      answer: null
+    },
+    {
+      question: "Question 3",
+      choices: [
+        {
+          sentence: "Choice 1",
+          evaluationScore: {
+            selfEsteemLevel: 0.7,
+            extroversionLevel: 0.8,
+            energyEmissionLevel: 0.9
+          }
+        },
+        // 必要に応じて他の選択肢を追加
+      ],
+      answer: null
+    }
+  ];
+  /**
+  * クイズアイテムの配列からスコアを計算します。
+  * 各クイズアイテムのanswerから評価スコアを取得し、それらの平均値を計算します。もし
+  * 問題に評価項目が含まれていなかったら評価スコアはnullが入ります。
+  * answerがnullのクイズアイテムが存在する場合、未解答があると判断し処理を中断しfalseを返します。
+  *
+  * @param quizItems スコアを計算するためのクイズアイテムの配列
+  * @returns 計算されたスコアのオブジェクト。ただし、任意のクイズアイテムのanswerがnullの場合はfalseを返します。
+  */
   function calculateScore(quizItems: QuizItem[]): EvaluationCriteria | false {
     const notNullScores: Record<keyof EvaluationCriteria, number[]> = {
       selfEsteemLevel: [],
@@ -35,7 +90,7 @@ function App() {
       energyEmissionLevel: null,
     };
     for (const item of quizItems) {
-      if (item.answer === undefined) {
+      if (item.answer === null) {
         return false;
       } else {
         Object.keys(item.answer.evaluationScore).forEach(key => {
@@ -65,8 +120,8 @@ function App() {
         quizItems.map((quizItem, index) => (
           <section key={index}>
             <article>{quizItem.question}</article>
-            {Object.entries(quizItem.choices).map(([key, value]) => (
-              <button key={key} onClick={onSubmit}>{value.sentence}</button>
+            {Object.entries(quizItem.choices).map(([key, choice]) => (
+              <button key={key} onClick={() => { quizItem.answer = choice }}>{choice.sentence}</button>
             ))}
           </section>
         ))
