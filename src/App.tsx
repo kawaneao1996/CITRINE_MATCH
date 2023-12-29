533c088982500f0d1549ee201ee6a97b20093dcb
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2023-12-29 10:26:01 +0900

    エラーページのルートを設定がまだ

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import QuestionaireTemplate from './templates/QuetionaireTemplate';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home!</div>,
+  },
+  {
+    path: "/questionaire",
+    element: <QuestionaireTemplate />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<>Home</>} />
-        <Route path='Questionaire' element={<QuestionaireTemplate />} />
-      </Routes>
+      <RouterProvider router={router} />
     </>
   );
 }
