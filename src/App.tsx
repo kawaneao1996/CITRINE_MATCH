eb373d7caac719e998cee2d903f0a2fb55987e10
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2024-01-16 20:48:19 +0900

    特に変更なし（不要なimport削除)\）

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import { Outlet } from "react-router-dom";
 import Header from './organisms/Header';
-import { BACKGROUND_THEME } from './utils/theme';
 function App() {
   return (
     <>
