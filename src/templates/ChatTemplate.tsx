10554b45a24988bdd5202a1b5d31fe76b3feeb37
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2024-01-25 05:37:15 +0900

    チャット画面のレイアウト暫定

--- src/templates/ChatTemplate.tsx
+++ src/templates/ChatTemplate.tsx
@@ -17,14 +17,14 @@ export default function ChatTemplate() {
 
     return (
         <>
-            <div className={`${BACKGROUND_THEME} w-full`}>
-                <div className="h-full  p-4 border-2 border-white rounded-xl">
-                    <ul className="w-full whitespace-pre-wrap break-all text-white">
+            <div className={`${BACKGROUND_THEME} w-full flex flex-col`}>
+                <div className="flex-grow-1 flex-shrink-1 overflow-auto border-2 border-white rounded-xl w-full ">
+                    <ul className="w-full p-2 whitespace-pre-wrap break-all text-white">
                         <li className="flex w-full  mb-4">
                             {/* ここにアイコンを表示 */}
                             <div className="max-w-full">
-                                <div className="text-sm font-bold">John Doe</div>
-                                <div className="text-sm ">
+                                <div className="text-sm ">John Doe</div>
+                                <div className="text-sm font-bold">
                                     Hey, how are you? I'm fine thank you. Nice to meet you! Shall
                                     we dance?
                                 </div>
@@ -33,24 +33,83 @@ export default function ChatTemplate() {
                         <li className="max-w-full  mb-4">
                             {/* ここにアイコンを表示 */}
                             <div className="max-w-full">
-                                <div className="text-sm font-bold">漆黒の堕天使</div>
-                                <div className="text-sm ">闇より生まれた死の天使参上</div>
+                                <div className="text-sm ">漆黒の堕天使</div>
+                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
+                            </div>
+                        </li>
+                        <li className="max-w-full  mb-4">
+                            {/* ここにアイコンを表示 */}
+                            <div className="max-w-full">
+                                <div className="text-sm ">漆黒の堕天使</div>
+                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
+                            </div>
+                        </li>
+                        <li className="max-w-full  mb-4">
+                            {/* ここにアイコンを表示 */}
+                            <div className="max-w-full">
+                                <div className="text-sm ">漆黒の堕天使</div>
+                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
+                            </div>
+                        </li>
+                        <li className="max-w-full  mb-4">
+                            {/* ここにアイコンを表示 */}
+                            <div className="max-w-full">
+                                <div className="text-sm ">漆黒の堕天使</div>
+                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
+                            </div>
+                        </li>
+                        <li className="max-w-full  mb-4">
+                            {/* ここにアイコンを表示 */}
+                            <div className="max-w-full">
+                                <div className="text-sm ">漆黒の堕天使</div>
+                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
+                            </div>
+                        </li>
+                        <li className="max-w-full  mb-4">
+                            {/* ここにアイコンを表示 */}
+                            <div className="max-w-full">
+                                <div className="text-sm ">漆黒の堕天使</div>
+                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
+                            </div>
+                        </li>
+                        <li className="max-w-full  mb-4">
+                            {/* ここにアイコンを表示 */}
+                            <div className="max-w-full">
+                                <div className="text-sm ">漆黒の堕天使</div>
+                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
+                            </div>
+                        </li>
+                        <li className="max-w-full  mb-4">
+                            {/* ここにアイコンを表示 */}
+                            <div className="max-w-full">
+                                <div className="text-sm ">漆黒の堕天使</div>
+                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
+                            </div>
+                        </li>
+                        <li className="max-w-full  mb-4">
+                            {/* ここにアイコンを表示 */}
+                            <div className="max-w-full">
+                                <div className="text-sm ">漆黒の堕天使</div>
+                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
                             </div>
                         </li>
                     </ul>
-                    {/* テキストフィールドのフォーム */}
-
-                    <form onSubmit={handleSubmit}>
+                </div>
+                {/* テキストフィールドのフォーム */}
+                <div className="flex flex-grow-0 flex-shrink-0 w-full mt-1 ">
+                    <form onSubmit={handleSubmit}
+                        className="flex w-full"
+                    >
                         <input
                             type="text"
                             value={message}
                             onChange={handleMessageChange}
                             placeholder="Type your message..."
-                            className="w-full p-2 mt-4 border-2 border-white rounded-lg"
+                            className="flex-grow p-2  border-2 border-white rounded-lg"
                         />
                         <button
                             type="submit"
-                            className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-lg"
+                            className="flex-grow-0  px-4 py-2 bg-secondary-500 text-white rounded-lg"
                         >
                             Send
                         </button>
