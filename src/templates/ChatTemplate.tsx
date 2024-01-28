d3a0d1de5931f77139fb4ab0ed7e3406d006a37d
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2024-01-28 19:15:54 +0900

    メッセージの型を変更

--- src/templates/ChatTemplate.tsx
+++ src/templates/ChatTemplate.tsx
@@ -4,25 +4,26 @@ import { BACKGROUND_THEME_CHAT } from "../utils/theme";
 export default function ChatTemplate() {
     const myId = "1";
     const myName = "ユーザー１";
-    type Message = {userId: string, userName: string, message:string};
-    const [message, setMessage] = useState<Message>();
+    type Message = {userId: string, userName: string, message?:string};
 
-    const [viewMessages, setViewMessages] = useState<(string | undefined)[]>([]);
+    const [currentMessage, setCurrentMessage] = useState<Message>({userId: myId, userName: myName, message: ""});
+
+    const [Messages, setMessages] = useState<Message[]>([]);
 
 
 
     const handleMessageChange = (e: React.ChangeEvent<HTMLInputElement>, userId:string, userName:string) => {
-        setMessage({userId: userId, userName: userName, message: e.target.value});
+        setCurrentMessage({userId: userId, userName: userName, message: e.target.value});
     };
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         // メッセージ送信の処理を実装する
-        console.log("Sending message:", message);
+        console.log("Sending message:", currentMessage);
 
-        setViewMessages([...viewMessages, message?.message]);
+        setMessages([...Messages, currentMessage]);
         // メッセージ入力欄を空にする
-        setMessage({userId: myId, userName: myName, message: ""});
+        setCurrentMessage({userId: myId, userName: myName, message: ""});
     };
 
     return (
@@ -30,28 +31,12 @@ export default function ChatTemplate() {
             <div className={`${BACKGROUND_THEME_CHAT} w-full flex flex-col`}>
                 <div className="flex-grow-1 flex-shrink-1 overflow-auto border-2 border-white rounded-xl w-full ">
                     <ul className="h-auto min-h-screen w-full p-2 whitespace-pre-wrap break-all text-white">
-                        <li className="flex w-full  mb-4">
-                            {/* ここにアイコンを表示 */}
-                            <div className="max-w-full">
-                                <div className="text-sm ">ユーザー１</div>
-                                <div className="text-sm font-bold">
-                                    このチャットは作りかけです。
-                                </div>
-                            </div>
-                        </li>
-                        <li className="max-w-full  mb-4">
-                            {/* ここにアイコンを表示 */}
-                            <div className="max-w-full">
-                                <div className="text-sm ">ユーザー2</div>
-                                <div className="text-sm font-bold">これから通信機能を実装します</div>
-                            </div>
-                        </li>
-                        {viewMessages.map((message, index) => (
+                        {Messages.map((message, index) => (
                             <li key={index} className="max-w-full  mb-4">
                                 {/* ここにアイコンを表示 */}
                                 <div className="max-w-full">
-                                    <div className="text-sm ">テストメッセージ{index + 1}</div>
-                                    <div className="text-sm font-bold">{message}</div>
+                                    <div className="text-sm ">{message.userName}</div>
+                                    <div className="text-sm font-bold">{message.message}</div>
                                 </div>
                             </li>
                         ))}
@@ -64,7 +49,7 @@ export default function ChatTemplate() {
                     >
                         <input
                             type="text"
-                            value={message?.message}
+                            value={currentMessage?.message}
                             onChange={(e) => handleMessageChange(e, myId, myName)}
                             // placeholder="Type your message..."
                             className="flex-grow p-2 mr-1 border-2 border-white rounded-lg"
