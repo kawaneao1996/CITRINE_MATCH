0a8937d863311804cbc179dfa3be8dc31faf2738
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2024-01-28 14:20:52 +0900

    見せるよう

--- src/templates/ChatTemplate.tsx
+++ src/templates/ChatTemplate.tsx
@@ -4,6 +4,10 @@ import { BACKGROUND_THEME } from "../utils/theme";
 export default function ChatTemplate() {
     const [message, setMessage] = useState("");
 
+    const [viewMessages, setViewMessages] = useState<string[]>([]);
+
+
+
     const handleMessageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setMessage(e.target.value);
     };
@@ -12,86 +16,47 @@ export default function ChatTemplate() {
         e.preventDefault();
         // メッセージ送信の処理を実装する
         console.log("Sending message:", message);
+
+        setViewMessages([...viewMessages, message]);
+        // メッセージ入力欄を空にする
         setMessage("");
     };
 
     return (
         <>
-        {/* TODO gridでここのデザインを指定する */}
             <div className={`${BACKGROUND_THEME} w-full flex flex-col`}>
                 <div className="flex-grow-1 flex-shrink-1 overflow-auto border-2 border-white rounded-xl w-full ">
                     <ul className="h-auto min-h-screen w-full p-2 whitespace-pre-wrap break-all text-white">
                         <li className="flex w-full  mb-4">
                             {/* ここにアイコンを表示 */}
                             <div className="max-w-full">
-                                <div className="text-sm ">John Doe</div>
+                                <div className="text-sm ">ユーザー１</div>
                                 <div className="text-sm font-bold">
-                                    Hey, how are you? I'm fine thank you. Nice to meet you! Shall
-                                    we dance?
+                                    このチャットは作りかけです。
                                 </div>
                             </div>
                         </li>
                         <li className="max-w-full  mb-4">
                             {/* ここにアイコンを表示 */}
                             <div className="max-w-full">
-                                <div className="text-sm ">漆黒の堕天使</div>
-                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
+                                <div className="text-sm ">ユーザー2</div>
+                                <div className="text-sm font-bold">これから通信機能を実装します</div>
                             </div>
                         </li>
+                        {viewMessages.map((message, index) => (
+                            <li key={index} className="max-w-full  mb-4">
+                                {/* ここにアイコンを表示 */}
+                                <div className="max-w-full">
+                                    <div className="text-sm ">テストメッセージ{index + 1}</div>
+                                    <div className="text-sm font-bold">{message}</div>
+                                </div>
+                            </li>
+                        ))}
                         <li className="max-w-full  mb-4">
                             {/* ここにアイコンを表示 */}
                             <div className="max-w-full">
-                                <div className="text-sm ">漆黒の堕天使</div>
-                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
-                            </div>
-                        </li>
-                        <li className="max-w-full  mb-4">
-                            {/* ここにアイコンを表示 */}
-                            <div className="max-w-full">
-                                <div className="text-sm ">漆黒の堕天使</div>
-                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
-                            </div>
-                        </li>
-                        <li className="max-w-full  mb-4">
-                            {/* ここにアイコンを表示 */}
-                            <div className="max-w-full">
-                                <div className="text-sm ">漆黒の堕天使</div>
-                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
-                            </div>
-                        </li>
-                        <li className="max-w-full  mb-4">
-                            {/* ここにアイコンを表示 */}
-                            <div className="max-w-full">
-                                <div className="text-sm ">漆黒の堕天使</div>
-                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
-                            </div>
-                        </li>
-                        <li className="max-w-full  mb-4">
-                            {/* ここにアイコンを表示 */}
-                            <div className="max-w-full">
-                                <div className="text-sm ">漆黒の堕天使</div>
-                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
-                            </div>
-                        </li>
-                        <li className="max-w-full  mb-4">
-                            {/* ここにアイコンを表示 */}
-                            <div className="max-w-full">
-                                <div className="text-sm ">漆黒の堕天使</div>
-                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
-                            </div>
-                        </li>
-                        <li className="max-w-full  mb-4">
-                            {/* ここにアイコンを表示 */}
-                            <div className="max-w-full">
-                                <div className="text-sm ">漆黒の堕天使</div>
-                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
-                            </div>
-                        </li>
-                        <li className="max-w-full  mb-4">
-                            {/* ここにアイコンを表示 */}
-                            <div className="max-w-full">
-                                <div className="text-sm ">漆黒の堕天使</div>
-                                <div className="text-sm font-bold">闇より生まれた死の天使参上</div>
+                                <div className="text-sm ">テスト</div>
+                                <div className="text-sm font-bold">{message}</div>
                             </div>
                         </li>
                     </ul>
@@ -105,7 +70,7 @@ export default function ChatTemplate() {
                             type="text"
                             value={message}
                             onChange={handleMessageChange}
-                            placeholder="Type your message..."
+                            // placeholder="Type your message..."
                             className="flex-grow p-2  border-2 border-white rounded-lg"
                         />
                         <button
