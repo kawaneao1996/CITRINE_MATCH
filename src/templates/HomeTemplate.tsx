af6f3fe9933b184c125b7b1d1cc9ccd4f3d5e1df
Kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2024-02-04 20:28:47 +0900

    ホーム画面にチャットを始めるボタンを追加

--- src/templates/HomeTemplate.tsx
+++ src/templates/HomeTemplate.tsx
@@ -1,11 +1,12 @@
 import { BACKGROUND_THEME_HOME } from "../utils/theme";
 import NavButton from "../elements/NavButton";
-import { ROUTER_QUIZ_URL } from "../utils/URL";
+import { ROUTER_CHAT_URL, ROUTER_QUIZ_URL } from "../utils/URL";
 
 export default function HomeTemplate() {
     return (
-        <div className={`${BACKGROUND_THEME_HOME} flex flex-1 justify-center items-center`}>
+        <div className={`${BACKGROUND_THEME_HOME} flex flex-1 flex-col justify-center items-center`}>
             <NavButton path={ROUTER_QUIZ_URL} label="心理テストを始める" />
+            <NavButton path={ROUTER_CHAT_URL} label="チャットを始める" />
         </div>
     );
 }
