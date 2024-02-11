09c7c66caeda59722def2c8d19c5969219fb8746
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2024-02-11 15:04:45 +0900

    ブラウザで再読み込みしないと初期メッセージが取得できない

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,13 +1,16 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? 'https://proud-hare-52.deno.dev/' : 'http://localhost:3000';
-console.log("URL", URL);
+// const URL = process.env.NODE_ENV === 'production' ? 'https://proud-hare-52.deno.dev/' : 'http://localhost:3000';
 // const URL = process.env.NODE_ENV === 'production' ? 'my-production-URL' : 'https://proud-hare-52.deno.dev/';
 
 // const URL = "https://proud-hare-52.deno.dev/";
+const URL = "https://empty-owl-26-m12j9rvk66cj.deno.dev";
 
 // const URL = "http://localhost:3000";
+console.log("socket.ts");
+console.log("URL", URL);
+console.log("import.meta.env", import.meta.env);
 
 export const socket = io(URL, {auth: {
     serveroffset: 0,
