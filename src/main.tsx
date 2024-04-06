518ca16eddbb8c31851a5d8da9288ea6b2357059
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2024-04-06 16:28:39 +0900

    https://github.com/kawaneao1996/PSYCHO_TEST_VITE/issues/14

--- src/main.tsx
+++ src/main.tsx
@@ -37,7 +37,7 @@ const router = createHashRouter([
   },
 ]);
 
-const supabase = createClient('https://sfjhnjthiyrjexayqpci.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmamhuanRoaXlyamV4YXlxcGNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIzODM2MDIsImV4cCI6MjAyNzk1OTYwMn0.SOBVY4Zdg8t2bBMjTkg7TWN-_lVAnbQF2UJKLrdOCL4');
+export const supabase = createClient('https://sfjhnjthiyrjexayqpci.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmamhuanRoaXlyamV4YXlxcGNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIzODM2MDIsImV4cCI6MjAyNzk1OTYwMn0.SOBVY4Zdg8t2bBMjTkg7TWN-_lVAnbQF2UJKLrdOCL4');
 
 function AppAuth() {
   const [session, setSession] = useState<Session | null>(null)
@@ -61,7 +61,7 @@ function AppAuth() {
       <Auth
         supabaseClient={supabase}
         appearance={{ theme: ThemeSupa }}
-        providers={['google', 'twitter',]}
+        providers={[]}
       />
     )
   }
--- src/organisms/Header.tsx
+++ src/organisms/Header.tsx
@@ -1,10 +1,15 @@
 import { BUTTON_THEME, BUTTON_THEME_ACTIVE, GRADATION_ANIMATE_THEME } from "../utils/theme"; import NavButton from "../elements/NavButton";
 import { ROUTER_BASE_URL, ROUTER_CHAT_URL } from "../utils/URL";
+import { supabase } from "../main";
 
 export default function Header() {
     const generateActiveStyle = ({ isActive }: { isActive: boolean }) => {
-        let style = isActive ? BUTTON_THEME_ACTIVE : BUTTON_THEME;
+        const style = isActive ? BUTTON_THEME_ACTIVE : BUTTON_THEME;
         return style;
+    };
+
+    const handleLogout = async () => {
+        await supabase.auth.signOut({ scope: 'local' });
     };
 
     return (
@@ -14,6 +19,7 @@ export default function Header() {
                 <div className="flex flex-row justify-between items-center">
                     <div className="flex flex-row items-center center">
                         <div className="text-3xl font-bold text-white p-2">Quiz App</div>
+                        <button onClick={handleLogout} className="m-2 p-2 text-white hover:text-primary-600 hover:bg-white border-2 rounded-lg font-bold text-lg">Log out</button>
                     </div>
                     <div className="flex flex-row justify-end items-center flex-grow">
                         <NavButton path={ROUTER_BASE_URL} className={generateActiveStyle} label="Home" />
