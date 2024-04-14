8c478be375083d09ce60da4d18ede1d6ea9ec973
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2024-04-14 10:18:32 +0900

    認証系のファイル切り分け

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React, {  } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {
   createHashRouter,
-  RouterProvider,
 } from "react-router-dom";
 import HomeTemplate from './templates/HomeTemplate.tsx'
 import QuestionnaireTemplate from './templates/QuestionnaireTemplate.tsx'
 import { ROUTER_BASE_URL, ROUTER_CHAT_URL, ROUTER_QUIZ_URL } from './utils/URL.ts';
 import ChatTemplate from './templates/ChatTemplate.tsx';
 import ErrorPage from './pages/ErrorPage.tsx';
-import { Session, createClient } from '@supabase/supabase-js'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { AppAuth } from './utils/AuthApp.tsx';
 
 const router = createHashRouter([
   {
@@ -37,44 +34,11 @@ const router = createHashRouter([
   },
 ]);
 
-export const supabase = createClient('https://sfjhnjthiyrjexayqpci.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmamhuanRoaXlyamV4YXlxcGNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIzODM2MDIsImV4cCI6MjAyNzk1OTYwMn0.SOBVY4Zdg8t2bBMjTkg7TWN-_lVAnbQF2UJKLrdOCL4');
 
-function AppAuth() {
-  const [session, setSession] = useState<Session | null>(null)
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-
-    return () => subscription.unsubscribe()
-  }, [])
-
-  if (!session) {
-    return (
-      <Auth
-        supabaseClient={supabase}
-        appearance={{ theme: ThemeSupa }}
-        providers={[]}
-      />
-    )
-  }
-  else {
-    console.log(session);
-    return (<RouterProvider router={router} />
-    )
-  }
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppAuth />
+    <AppAuth router={router} />
   </React.StrictMode>,
 )
 
---
+++ src/utils/AuthApp.tsx
@@ -0,0 +1,38 @@
+import { Router } from "@remix-run/router";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import { RouterProvider } from "react-router-dom";
+import { supabase } from "./supabaseClient";
+
+export function AppAuth(props: { router: Router }) {
+    const [session, setSession] = useState<Session | null>(null);
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session);
+        });
+
+        const {
+            data: { subscription },
+        } = supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session);
+        });
+
+        return () => subscription.unsubscribe();
+    }, []);
+
+    if (!session) {
+        return (
+            <Auth
+                supabaseClient={supabase}
+                appearance={{ theme: ThemeSupa }}
+                providers={[]}
+            />
+        );
+    } else {
+        console.log(session);
+        return <RouterProvider router={props.router} />;
+    }
+}
