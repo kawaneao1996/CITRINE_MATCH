9291785992fa488dc28e41cf7be386d74f151408
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2024-04-21 13:49:33 +0900

    AuthAppの修正とログイン画面のデザイン変更

--- src/utils/AuthApp.tsx
+++ src/utils/AuthApp.tsx
@@ -1,11 +1,9 @@
 import { Router } from "@remix-run/router";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { Session } from "@supabase/supabase-js";
-import { useEffect, useState } from "react";
-import { RouterProvider } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 import { supabase } from "./supabaseClient";
-import { AUTH_APP_LABELS } from "./AuthAppLabels";
+import { RouterProvider } from "react-router-dom";
+import { BACKGROUND_THEME_LOGIN, GRADATION_ANIMATE_THEME_LOGIN } from "./theme";
 
 export function AppAuth(props: { router: Router }) {
     const [session, setSession] = useState<Session | null>(null);
@@ -24,18 +22,55 @@ export function AppAuth(props: { router: Router }) {
         return () => subscription.unsubscribe();
     }, []);
 
+    const tabContents = {
+        '初めての方': 'first',
+        'すでにアカウントをお持ちの方': 'login',
+    } as const;
+    const tabs = Object.keys(tabContents);
+    type Tabs = typeof tabs[number];
+    const [activeTab, setActiveTab] = useState<Tabs>(tabs[0]);
+    // ヘッダーの高さに合わせてコンテンツの高さを調整する
+    const headerRef = useRef<HTMLElement>(null);
+    const [headerHeight, setHeaderHeight] = useState(0);
+
+    useEffect(() => {
+        if (headerRef.current) {
+            setHeaderHeight(headerRef.current.clientHeight);
+        }
+    }, []);
+
     if (!session) {
         return (
-            <Auth
-                supabaseClient={supabase}
-                appearance={{ theme: ThemeSupa }}
-                providers={[]}
-                // providers={["google", "twitter"]}
-                localization={AUTH_APP_LABELS}
-            />
+            <>
+                <header className={`${GRADATION_ANIMATE_THEME_LOGIN} h-auto`} ref={headerRef}>
+                    <ul className="flex flex-wrap -mb-px text-sm font-medium text-center" role="tablist">
+                        {tabs.map(tab => {
+                            return (
+                                <li className="me-2" role="presentation">
+                                    <button
+                                        className={`inline-block p-4 border-b-2 text-sm rounded-t-lg ${activeTab === tab ? 'text-purple-600 border-purple-600' : 'text-gray-500 border-transparent'}`}
+                                        onClick={() => setActiveTab(tab)}
+                                        role="tab"
+                                    >
+                                        {tab}
+                                    </button>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </header>
+                <div className={`${BACKGROUND_THEME_LOGIN}`} style={{ minHeight: `calc(100vh - ${headerHeight}px)` }} id="login-tabs">
+                    {Object.entries(tabContents).map(([tab, content]) => {
+                        return (
+                            <div className={`${activeTab !== tab ? 'hidden' : ''}`} role="tabpanel">
+                                {content}
+                            </div>
+                        )
+                    })}
+                </div>
+            </>
         );
     } else {
-        console.log(session);
         return <RouterProvider router={props.router} />;
     }
 }
--- src/utils/theme.tsx
+++ src/utils/theme.tsx
@@ -1,6 +1,7 @@
 export const BACKGROUND_THEME_HOME = `bg-primary-600 h-screen-minus-header p-3`;
 export const BACKGROUND_THEME_QUESTION = `bg-primary-600 `;
 export const BACKGROUND_THEME_CHAT = `bg-primary-600 h-screen-minus-header p-3`;
+export const BACKGROUND_THEME_LOGIN = `bg-secondary-200  p-3`;
 export const BUTTON_THEME = `
 m-2
 p-2
@@ -30,3 +31,9 @@ to-primary-700
 background-animate
 `;
 export const HEADER_HEIGHT = '16';
+export const GRADATION_ANIMATE_THEME_LOGIN = `
+bg-gradient-to-r
+from-cyan-400
+to-secondary-500
+background-animate
+`;
